96cf502713b01f3006c4e918df9c419f8a98ff56
Ahmet Bekcan <ahmetbekcan26@gmail.com>
2025-06-12 23:56:30 +0300

    bug fix

--- src/processing_cnngemma.py
+++ src/processing_cnngemma.py
@@ -91,7 +91,8 @@ class CNNGemmaProcessor:
                 return_token_type_ids=True
             )
         return_data = {"pixel_values": pixel_values, **inputs}
-        labels = inputs["input_ids"].masked_fill(inputs["token_type_ids"] == 0, -100)
+        if (inputs["token_type_ids"] != None):
+            labels = inputs["input_ids"].masked_fill(inputs["token_type_ids"] == 0, -100)
         return_data.update({"labels": labels})
 
         return return_data
